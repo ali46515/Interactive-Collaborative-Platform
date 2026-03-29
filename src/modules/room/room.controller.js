c571f797754f8a45102a05b017345cf2b74833be
ali46515 <aliarshad.9474@gmail.com>
2026-03-29 22:46:47 +0500

    Implemented remaining room contollers

--- src/modules/room/room.controller.js
+++ src/modules/room/room.controller.js
@@ -16,9 +16,41 @@ const joinRoom = asyncHandler(async (req, res) => {
   return success(res, 200, "Joined room", { room });
 });
 
+const listRooms = asyncHandler(async (req, res) => {
+  const rooms = await roomService.listUserRooms(req.user._id);
+  return success(res, 200, "Rooms fetched", { rooms });
+});
+
+const getRoom = asyncHandler(async (req, res) => {
+  const room = await roomService.getRoomBySlug(req.params.slug);
+  return success(res, 200, "Room fetched", { room });
+});
+
 const leaveRoom = asyncHandler(async (req, res) => {
   await roomService.leaveRoom(req.user._id, req.params.id);
   return success(res, 200, "Left room");
 });
 
-export { createRoom, joinRoom, leaveRoom };
+const updateRoom = asyncHandler(async (req, res) => {
+  const room = await roomService.updateRoom(
+    req.user._id,
+    req.params.id,
+    req.body,
+  );
+  return success(res, 200, "Room updated", { room });
+});
+
+const archiveRoom = asyncHandler(async (req, res) => {
+  await roomService.archiveRoom(req.user._id, req.params.id);
+  return success(res, 200, "Room archived");
+});
+
+export {
+  createRoom,
+  listRooms,
+  getRoom,
+  joinRoom,
+  leaveRoom,
+  updateRoom,
+  archiveRoom,
+};
